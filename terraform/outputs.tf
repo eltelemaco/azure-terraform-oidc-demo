5749a1b376d92ec234c51d2363af2d8a8cc0c0b5
Luis Antonio Reynoso <telemaco_81@hotmail.com>
2025-03-05 04:18:02 -0600

    Amazing workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,31 +11,19 @@ output "azure_tenant_id" {
   sensitive   = true
 }
 
-output "azure_client_id" {
-  description = "The Azure Client ID (Application ID) for OIDC authentication"
-  value       = azuread_application.az_application.client_id
-  sensitive   = true
-}
-
-# Resource group outputs
-output "resource_group_name" {
-  description = "The name of the Azure resource group"
-  value       = azurerm_resource_group.rg.name
-}
-
-output "resource_group_location" {
-  description = "The location of the Azure resource group"
-  value       = azurerm_resource_group.rg.location
-}
-
-# GitHub OIDC configuration outputs
-output "github_oidc_application_name" {
-  description = "The name of the Azure AD application for GitHub OIDC"
-  value       = azuread_application.az_application.display_name
-}
-
-output "github_repository" {
-  description = "The GitHub repository configured for OIDC"
-  value       = var.github_repository
-}
+# # Resource group outputs
+# output "azurerm_resource_group" {
+#   description = "The name of the Azure resource group"
+#   value       = azurerm_resource_group.name
+# }
+#
+# output "resource_group_location" {
+#   description = "The location of the Azure resource group"
+#   value       = azurerm_resource_group.location
+# }
+#
+# output "github_repository" {
+#   description = "The GitHub repository configured for OIDC"
+#   value       = var.github_repository
+# }
 
