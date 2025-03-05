521be9a856ff84069dfa2798d2f7c4dc8808870d
Luis Antonio Reynoso <telemaco_81@hotmail.com>
2025-03-05 05:12:24 -0600

    Amazing workflow

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -50,14 +50,14 @@ locals {
     workload    = var.resource_name_workload
     environment = var.resource_name_environment
     location    = var.location
-    sequence = format("%03d", var.resource_name_sequence_start)
+    sequence    = format("%03d", var.resource_name_sequence_start)
   }
 
   resource_names = {
-    for key, value in var.resource_name_templates : key => templatestring(value, local.name_replacements)}
+  for key, value in var.resource_name_templates : key => templatestring(value, local.name_replacements) }
 }
 locals {
-    resource_group_name = var.resource_group_create ? module.azurerm_resource_group[0].name : var.resource_group_name
+  resource_group_name = var.resource_group_create ? module.azurerm_resource_group[0].name : var.resource_group_name
 }
 
 
