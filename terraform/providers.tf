7c08d9cadbb03efe64af15ce4f235666d6a88beb
Luis Antonio Reynoso <telemaco_81@hotmail.com>
2025-03-05 01:56:21 -0600

    Amazing workflow

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,20 +1,24 @@
 terraform {
   backend "azurerm" {}
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0, < 4.0.0"
+      version = ">= 3.0.2, < 4.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-
   # OIDC Authentication configuration
   use_oidc        = true
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg_name" {
+  name = var.resource_group_name
+  location = var.location
 }
 
